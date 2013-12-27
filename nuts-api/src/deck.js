cdba555976ce018c3ad3b4a1adfa9e712a8b9de9
Code R <code@rabiller.com>
2013-12-27 21:57:51 +0100

    Add some deck features

--- nuts-api/src/deck.js
+++ nuts-api/src/deck.js
@@ -1,5 +1,39 @@
+var Card = require("./card");
 
+var DeckHelpers = {
+	loadAllCards: function(){
+		var colors = ["Spade", "Club", "Diamond", "Heart"];
+		var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+		var cards = new Array();
+
+		for(var c = 0; c < colors.length; c++){
+			for (var v = 0; v < values.length; v++){
+				cards.push(new Card(values[v], colors[c]));
+			}
+		}
+
+		return DeckHelpers.shuffle(cards);
+	},
+
+	shuffle: function(cards){
+		for(var j, x, i = cards.length; i; j = Math.floor(Math.random() * i), x = cards[--i], cards[i] = cards[j], cards[j] = x);
+		{
+			return cards;
+		}
+	}
+};
 
 var Deck = function(){
+	this._cards = DeckHelpers.loadAllCards();
+};
 
-}
+Deck.prototype = {
+	getRemainingCardCount: function(){
+		return this._cards.length;
+	},
+	peekCard : function(){
+		return this._cards.pop();
+	}
+};
+
+module.exports = Deck;
--- nuts-api/test/deck_spec.js
+++ nuts-api/test/deck_spec.js
@@ -4,6 +4,19 @@ var Deck = require("../src/deck");
 describe('Deck', function(){
 	it('Doit contenir 52 cartes', function(){
 		var deck = new Deck();
-		
+		assert.equal(deck.getRemainingCardCount(), 52);
+	});
+
+	it('Doit donner une carte à la demande', function(){
+		var deck = new Deck();
+		var card = deck.peekCard();
+		assert(typeof card, "Card");
+	});
+
+	it('Ne doit jamais donner la même carte', function(){
+		var deck = new Deck();
+		var cardA = deck.peekCard();
+		var cardB = deck.peekCard();
+		assert.notDeepEqual(cardA, cardB);
 	});
 });
