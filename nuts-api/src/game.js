40a31030d31a522e21e75b68e3b3bd17e33f9661
Guillaume Vincent <vincent.guillaume.inp@gmail.com>
2014-03-24 20:39:18 +0100

    coding dojo 24 mars, refactoring try

--- nuts-api/src/game.js
+++ nuts-api/src/game.js
@@ -5,195 +5,196 @@ var Hand = require("./hand");
 var Deck = require("./deck");
 var Evaluator = require("../src/evaluator");
 
-var Game = function(players, gameCount){
-	if(_.isEmpty(players))
-		throw new Error("Game should have players.");
-	
-	this.players = players;
-	this.deck = new Deck();
-	this.step = 0;
+var Game = function (players, gameCount) {
+    if (_.isEmpty(players))
+        throw new Error("Game should have players.");
 
-	this.cards = [];
+    this.players = players;
+    this.deck = new Deck();
+    this.step = 0;
 
-	this.smallBlindAmount = 1;
-	this.bigBlindAmount = 2;
+    this.cards = [];
 
-	var index = gameCount < this.players.length ? gameCount : gameCount % this.players.length;
-	var next = index + 1 < this.players.length ? index + 1 : this.players.length - 1;
-	
-	this.smallBlindPlayer = this.players[index];
-	this.bigBlindPlayer = this.players[next];
-	
-	this.currentPlayer = this.players.length > 2 ? this.players[2] : this.players[0];
+    this.smallBlindAmount = 1;
+    this.bigBlindAmount = 2;
 
-	this.pot = 0;
+    var index = gameCount < this.players.length ? gameCount : gameCount % this.players.length;
+    var next = index + 1 < this.players.length ? index + 1 : this.players.length - 1;
+
+    this.smallBlindPlayer = this.players[index];
+    this.bigBlindPlayer = this.players[next];
+
+    this.currentPlayer = this.players.length > 2 ? this.players[2] : this.players[0];
+
+    this.pot = 0;
 };
 Game.prototype = extend({}, EventEmitter.prototype, {
-	start : function(){
-		this._betAmount(this.smallBlindPlayer, this.smallBlindAmount);
-		this.smallBlindPlayer.emit("SmallBlind", this.smallBlindAmount);
-		
-		this._betAmount(this.bigBlindPlayer, this.bigBlindAmount);
-		this.bigBlindPlayer.emit("BigBlind", this.bigBlindAmount);
-		
-		this._distribute();
+    start: function () {
+        this._betAmount(this.smallBlindPlayer, this.smallBlindAmount);
+        this.smallBlindPlayer.emit("SmallBlind", this.smallBlindAmount);
 
-		this.currentPlayer.actions = this._getAvailableActions();
-	},
+        this._betAmount(this.bigBlindPlayer, this.bigBlindAmount);
+        this.bigBlindPlayer.emit("BigBlind", this.bigBlindAmount);
 
-	nextToken: function(){
-		if (this.players.length == 1){
-			this.players[0].stackAmount += this.pot;
-			this.players[0].emit("Won", null);
-			this.emit("Finished");
-		}else{
-			var allPlayed = _.every(this.players, function(p) { return p.hasPlayed; });
-			var firstBet = this.players[0].betAmount;
-			var allSameAmount = _.every(this.players, function(p) { return firstBet == p.betAmount; });
-			
-			if(allPlayed && allSameAmount) {
-				this._loadBoard();
-			} else {
-				this.currentPlayer = this._getNextPlayer();
-				this.currentPlayer.actions = this._getAvailableActions();
-			}
-		}
-	},
+        this._distribute();
 
-	_distribute : function(){
-		for(var i = 0; i < this.players.length; i++){
-			var first = this.deck.peekCard();
-			var second = this.deck.peekCard();
-			var hand = new Hand(first, second);
-			this.players[i].addHand(hand);
-			this.players[i].hasPlayed = false;
-		}
-	},
+        this.currentPlayer.actions = this._getAvailableActions();
+    },
 
-	_loadBoard: function(){
-		this.step++;
-		switch(this.step){
-			case 1: // Flop
-				this.cards.push(this.deck.peekCard());
-				this.cards.push(this.deck.peekCard());
-				this.cards.push(this.deck.peekCard());
-				break;
+    everybodyHasBetTheSameAmount: function (players) {
+        var firstBet = this.players[0].betAmount;
+        var allSameAmount = _.every(players, function (p) {
+            return firstBet == p.betAmount;
+        });
+        return allSameAmount;
+    },
+    nextToken: function () {
+        if (this.players.length == 1) {
+            this.players[0].stackAmount += this.pot + this.players[0].betAmount;
 
-			case 2: // Turn
-				this.cards.push(this.deck.peekCard());
-				break;
+            this.players[0].emit("Won", null);
+            this.emit("Finished");
+        } else {
+            var allPlayed = _.every(this.players, function (p) {
+                return p.hasPlayed;
+            });
 
-			case 3: //River
-				this.cards.push(this.deck.peekCard());
-				break;
+            var allSameAmount = this.everybodyHasBetTheSameAmount(this.players);
 
-			default:
-				var winner = new Evaluator(this.cards, this.players).getWinner();
-				winner.player.stackAmount += this.pot;
-				winner.player.emit("Won", winner.combination);
-				this.emit("Finished");
-				break;
-		}
 
-		var pot = 0;
-		_.each(this.players, function(p){
-			pot += p.betAmount;
-			p.betAmount = 0;
-			p.hasPlayed = false;
-		});
-		this.pot += pot;
-		this.currentPlayer = this.smallBlindPlayer;
-		this.currentPlayer.actions = this._getAvailableActions();
-	},
+            if (allPlayed && allSameAmount) {
+                this._loadBoard();
+            } else {
+                this.currentPlayer = this._getNextPlayer();
+                this.currentPlayer.actions = this._getAvailableActions();
+            }
+        }
+    },
 
-	_getAvailableActions: function(){
-		var self = this;
-		var actions = {};
-		var previousPlayer = self._getPreviousPlayer();
+    _distribute: function () {
+        for (var i = 0; i < this.players.length; i++) {
+            var first = this.deck.peekCard();
+            var second = this.deck.peekCard();
+            var hand = new Hand(first, second);
+            this.players[i].addHand(hand);
+            this.players[i].hasPlayed = false;
+        }
+    },
 
-		var fold = function(){
-			self._pushToPot(self.currentPlayer);
+    _loadBoard: function () {
+        this.step++;
+        switch (this.step) {
+            case 1: // Flop
+                this.cards.push(this.deck.peekCard());
+                this.cards.push(this.deck.peekCard());
+                this.cards.push(this.deck.peekCard());
+                break;
 
-			self.players = _.without(self.players, self.currentPlayer);
+            case 2: // Turn
+                this.cards.push(this.deck.peekCard());
+                break;
 
-			self.currentPlayer.emit("Folded");
+            case 3: //River
+                this.cards.push(this.deck.peekCard());
+                break;
 
-			self.currentPlayer.actions = null;
-			self.nextToken();
-		};
+            default:
+                var winner = new Evaluator(this.cards, this.players).getWinner();
+                winner.player.stackAmount += this.pot;
+                winner.player.emit("Won", winner.combination);
+                this.emit("Finished");
+                break;
+        }
 
-		var bet = function(amount){
-			self._betAmount(self.currentPlayer, amount);
+        var pot = 0;
+        _.each(this.players, function (p) {
+            pot += p.betAmount;
+            p.betAmount = 0;
+            p.hasPlayed = false;
+        });
+        this.pot += pot;
+        this.currentPlayer = this.smallBlindPlayer;
+        this.currentPlayer.actions = this._getAvailableActions();
+    },
 
-			self.currentPlayer.emit("Bet");
+    moveToNextPlayer: function (signal) {
+        var self = this
+        self.currentPlayer.emit(signal);
+        self.currentPlayer.hasPlayed = (signal != 'Folded');
+        self.currentPlayer.actions = null;
+        self.nextToken();
+    },
 
-			self.currentPlayer.hasPlayed = true;
-			self.currentPlayer.actions = null;
-			self.nextToken();
-		};
+    _getAvailableActions: function () {
+        var self = this;
+        var actions = {};
+        var previousPlayer = self._getPreviousPlayer();
 
-		var call = function(){
-			self._betAmount(self.currentPlayer, previousPlayer.betAmount);
+        var fold = function () {
+            self._pushToPot(self.currentPlayer);
 
-			self.currentPlayer.emit("Called");
-			
-			self.currentPlayer.hasPlayed = true;
-			self.currentPlayer.actions = null;
-			self.nextToken();
-		};
+            self.players = _.without(self.players, self.currentPlayer);
+            self.moveToNextPlayer("Folded");
+        };
 
-		var check = function(){
-			self.currentPlayer.emit("Checked");
+        var bet = function (amount) {
+            self._betAmount(self.currentPlayer, amount);
+            self.moveToNextPlayer("Bet")
+        };
 
-			self.currentPlayer.hasPlayed = true;
-			self.currentPlayer.actions = null;
-			self.nextToken();
-		};
+        var call = function () {
+            self._betAmount(self.currentPlayer, previousPlayer.betAmount);
+            self.moveToNextPlayer("Called")
+        };
 
-		actions["fold"] = fold;
+        var check = function () {
+            self.moveToNextPlayer("Checked")
+        };
 
-		if(self.currentPlayer.stackAmount > 0){
-			actions["bet"] = bet;
-		}
-		
-		if(previousPlayer.betAmount > self.currentPlayer.betAmount){
-			actions["call"] = call;
-		}
+        actions["fold"] = fold;
 
-		if(previousPlayer.betAmount === self.currentPlayer.betAmount) {
-			actions["check"] = check;
-		}
+        if (self.currentPlayer.stackAmount > 0) {
+            actions["bet"] = bet;
+        }
 
-		self.currentPlayer.emit("TokenObtained", { actions : actions, board : this.cards});
-		return actions;
-	},
+        if (previousPlayer.betAmount > self.currentPlayer.betAmount) {
+            actions["call"] = call;
+        }
 
-	_betAmount: function(player, amount){
-		amount = amount - player.betAmount;
-		if (player.stackAmount < amount){
-			amount = player.stackAmount;
-			player.emit("AllIn");
-		}
-		player.stackAmount -= amount;
-		player.betAmount += amount;
-	},
+        if (previousPlayer.betAmount === self.currentPlayer.betAmount) {
+            actions["check"] = check;
+        }
 
-	_pushToPot: function(player){
-		this.pot += player.betAmount;
-		player.betAmount = 0;
-	},
+        self.currentPlayer.emit("TokenObtained", { actions: actions, board: this.cards});
+        return actions;
+    },
 
-	_getPreviousPlayer: function(){
-		var currentIndex = _.indexOf(this.players, this.currentPlayer);
-		var previousIndex = (currentIndex - 1 < 0) ? this.players.length - 1 : currentIndex - 1;
-		return this.players[previousIndex];
-	},
+    _betAmount: function (player, amount) {
+        amount = amount - player.betAmount;
+        if (player.stackAmount < amount) {
+            amount = player.stackAmount;
+            player.emit("AllIn");
+        }
+        player.stackAmount -= amount;
+        player.betAmount += amount;
+    },
 
-	_getNextPlayer: function(){
-		var currentIndex = _.indexOf(this.players, this.currentPlayer);
-		var nextIndex = (currentIndex + 1 >= this.players.length) ? 0 : currentIndex + 1;
-		return this.players[nextIndex];
-	}
+    _pushToPot: function (player) {
+        this.pot += player.betAmount;
+        player.betAmount = 0;
+    },
+
+    _getPreviousPlayer: function () {
+        var currentIndex = _.indexOf(this.players, this.currentPlayer);
+        var previousIndex = (currentIndex - 1 < 0) ? this.players.length - 1 : currentIndex - 1;
+        return this.players[previousIndex];
+    },
+
+    _getNextPlayer: function () {
+        var currentIndex = _.indexOf(this.players, this.currentPlayer);
+        var nextIndex = (currentIndex + 1 >= this.players.length) ? 0 : currentIndex + 1;
+        return this.players[nextIndex];
+    }
 });
 
 module.exports = Game;
--- nuts-api/test/game_spec.js
+++ nuts-api/test/game_spec.js
@@ -3,235 +3,285 @@ var assert = require("assert");
 var Player = require("../src/player");
 var Game = require("../src/game");
 
-describe('Game', function(){
-	it("Doit lancer une exception s'il n'a pas de joueur", function(){
-		var buildGame = function(){ new Game(); };
-		assert.throws(buildGame);
-	});
+describe('Game', function () {
+    it("Doit lancer une exception s'il n'a pas de joueur", function () {
+        var buildGame = function () {
+            new Game();
+        };
+        assert.throws(buildGame);
+    });
 
-	it("Doit avoir des joueurs", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+    it("Doit avoir des joueurs", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		var game = new Game([a, b, c], 0);
-		assert.deepEqual(game.players, [a, b, c]);
-	});
+        var game = new Game([a, b, c], 0);
+        assert.deepEqual(game.players, [a, b, c]);
+    });
 
-	it("Doit prendre les blinds au démarrage du jeu", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+    it("Doit prendre les blinds au démarrage du jeu", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		var game = new Game([a, b, c], 0);
-		game.start();
+        var game = new Game([a, b, c], 0);
+        game.start();
 
-		assert.equal(a.stackAmount, 9);
-		assert.equal(a.betAmount, 1);
-		assert.equal(b.stackAmount, 8);
-		assert.equal(b.betAmount, 2);
-		assert.equal(c.stackAmount, 10);
-	});
+        assert.equal(a.stackAmount, 9);
+        assert.equal(a.betAmount, 1);
+        assert.equal(b.stackAmount, 8);
+        assert.equal(b.betAmount, 2);
+        assert.equal(c.stackAmount, 10);
+    });
 
-	it("Doit distribuer des cartes au démarrage du jeu", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+    it("Doit distribuer des cartes au démarrage du jeu", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		var game = new Game([a, b, c], 0);
-		game.start();
+        var game = new Game([a, b, c], 0);
+        game.start();
 
-		assert.notEqual(a.hand.firstCard, null);
-		assert.notEqual(a.hand.secondCard, null);
+        assert.notEqual(a.hand.firstCard, null);
+        assert.notEqual(a.hand.secondCard, null);
 
-		assert.notEqual(b.hand.firstCard, null);
-		assert.notEqual(b.hand.secondCard, null);
+        assert.notEqual(b.hand.firstCard, null);
+        assert.notEqual(b.hand.secondCard, null);
 
-		assert.notEqual(c.hand.firstCard, null);
-		assert.notEqual(c.hand.secondCard, null);
-	});
+        assert.notEqual(c.hand.firstCard, null);
+        assert.notEqual(c.hand.secondCard, null);
+    });
 
-	it("Doit lancer un tour de pari", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+    it("Doit lancer un tour de pari", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		var game = new Game([a, b, c], 0);
-		game.start();
-		
-		assert.equal(a.actions, null);
-		assert.equal(b.actions, null);
-		assert.notEqual(c.actions, null);
-	});
+        var game = new Game([a, b, c], 0);
+        game.start();
 
-	it("Doit distribuer le flop lorsque le tour de pari est fini", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+        assert.equal(a.actions, null);
+        assert.equal(b.actions, null);
+        assert.notEqual(c.actions, null);
+    });
 
-		var game = new Game([a, b, c], 0);
-		game.start();
-		
-		assert.equal(a.actions, null);
-		assert.equal(a.stackAmount, 9);
-		assert.equal(b.actions, null);
-		assert.equal(b.stackAmount, 8);
-		assert.notEqual(c.actions, null);
-		assert.equal(c.stackAmount, 10);
+    it("Doit distribuer le flop lorsque le tour de pari est fini", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		c.actions.fold();
+        var game = new Game([a, b, c], 0);
+        game.start();
 
-		assert.notEqual(a.actions, null);
-		assert.equal(a.stackAmount, 9);
-		assert.equal(b.actions, null);
-		assert.equal(b.stackAmount, 8);
-		assert.equal(c.actions, null);
-		assert.equal(c.stackAmount, 10);
+        assert.equal(a.actions, null);
+        assert.equal(a.stackAmount, 9);
+        assert.equal(b.actions, null);
+        assert.equal(b.stackAmount, 8);
+        assert.notEqual(c.actions, null);
+        assert.equal(c.stackAmount, 10);
 
-		a.actions.call();
+        c.actions.fold();
 
-		assert.equal(a.actions, null);
-		assert.equal(a.stackAmount, 8);
-		assert.notEqual(b.actions, null);
-		assert.equal(b.stackAmount, 8);
-		assert.equal(c.actions, null);
-		assert.equal(c.stackAmount, 10);
+        assert.notEqual(a.actions, null);
+        assert.equal(a.stackAmount, 9);
+        assert.equal(b.actions, null);
+        assert.equal(b.stackAmount, 8);
+        assert.equal(c.actions, null);
+        assert.equal(c.stackAmount, 10);
 
-		b.actions.check();
+        a.actions.call();
 
-		assert.equal(game.cards.length, 3);
-		assert.equal(game.pot, 4);
+        assert.equal(a.actions, null);
+        assert.equal(a.stackAmount, 8);
+        assert.notEqual(b.actions, null);
+        assert.equal(b.stackAmount, 8);
+        assert.equal(c.actions, null);
+        assert.equal(c.stackAmount, 10);
 
-		assert.notEqual(a.actions, null);
-		assert.equal(a.stackAmount, 8);
-		assert.equal(b.actions, null);
-		assert.equal(b.stackAmount, 8);
-		assert.equal(c.actions, null);
-		assert.equal(c.stackAmount, 10);
-	});
+        b.actions.check();
 
-	it("Doit distribuer le turn lorsque le tour de pari après flop est fini", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+        assert.equal(game.cards.length, 3);
+        assert.equal(game.pot, 4);
 
-		var game = new Game([a, b, c], 0);
-		game.start();
-		c.actions.fold();
-		a.actions.call();
-		b.actions.check();
+        assert.notEqual(a.actions, null);
+        assert.equal(a.stackAmount, 8);
+        assert.equal(b.actions, null);
+        assert.equal(b.stackAmount, 8);
+        assert.equal(c.actions, null);
+        assert.equal(c.stackAmount, 10);
+    });
 
-		assert.equal(game.cards.length, 3);
+    it("Doit distribuer le turn lorsque le tour de pari après flop est fini", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		a.actions.check();
-		b.actions.bet(2);
+        var game = new Game([a, b, c], 0);
+        game.start();
+        c.actions.fold();
+        a.actions.call();
+        b.actions.check();
 
-		assert.notEqual(a.actions, null);
-		assert.equal(a.stackAmount, 8);
-		assert.equal(b.actions, null);
-		assert.equal(b.stackAmount, 6);
+        assert.equal(game.cards.length, 3);
 
-		a.actions.call();
+        a.actions.check();
+        b.actions.bet(2);
 
-		assert.equal(game.cards.length, 4);
-		assert.equal(game.pot, 8);
+        assert.notEqual(a.actions, null);
+        assert.equal(a.stackAmount, 8);
+        assert.equal(b.actions, null);
+        assert.equal(b.stackAmount, 6);
 
-		assert.notEqual(a.actions, null);
-		assert.equal(a.stackAmount, 6);
-		assert.equal(b.actions, null);
-		assert.equal(b.stackAmount, 6);
-	});
+        a.actions.call();
 
-	it("Doit distribuer le river lorsque le tour de pari après turn est fini", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+        assert.equal(game.cards.length, 4);
+        assert.equal(game.pot, 8);
 
-		var game = new Game([a, b, c], 0);
-		game.start();
+        assert.notEqual(a.actions, null);
+        assert.equal(a.stackAmount, 6);
+        assert.equal(b.actions, null);
+        assert.equal(b.stackAmount, 6);
+    });
 
-		//Flop
-		c.actions.fold();
-		a.actions.call();
-		b.actions.check();
+    it("Doit distribuer le river lorsque le tour de pari après turn est fini", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		//Turn
-		a.actions.check();
-		b.actions.bet(2);
-		a.actions.call();
+        var game = new Game([a, b, c], 0);
+        game.start();
 
-		assert.equal(game.cards.length, 4);
-		assert.equal(game.pot, 8);
+        //Flop
+        c.actions.fold();
+        a.actions.call();
+        b.actions.check();
 
-		a.actions.check();
-		b.actions.check();
+        //Turn
+        a.actions.check();
+        b.actions.bet(2);
+        a.actions.call();
 
-		assert.equal(game.cards.length, 5);
-		assert.equal(game.pot, 8);
+        assert.equal(game.cards.length, 4);
+        assert.equal(game.pot, 8);
 
-		assert.notEqual(a.actions, null);
-		assert.equal(a.stackAmount, 6);
-		assert.equal(b.actions, null);
-		assert.equal(b.stackAmount, 6);
-	});
+        a.actions.check();
+        b.actions.check();
 
-	it("Doit faire gagner celui qui a la combinaison la plus élevé", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+        assert.equal(game.cards.length, 5);
+        assert.equal(game.pot, 8);
 
-		var game = new Game([a, b, c], 0);
-		game.start();
+        assert.notEqual(a.actions, null);
+        assert.equal(a.stackAmount, 6);
+        assert.equal(b.actions, null);
+        assert.equal(b.stackAmount, 6);
+    });
 
-		//Pre-Flop
-		c.actions.fold();
-		a.actions.call();
-		b.actions.check();
-		//Flop
-		assert.equal(game.cards.length, 3);
-		a.actions.check();
-		b.actions.bet(2);
-		a.actions.call();
-		//Turn
-		assert.equal(game.cards.length, 4);
-		a.actions.check();
-		b.actions.check();
-		//River
-		assert.equal(game.cards.length, 5);
-		a.actions.check();
-		b.actions.check();
+    it("Doit faire gagner celui qui a la combinaison la plus élevé", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		assert.equal(game.pot, 8);
-		var winner = 14;
-		var looser = 6;
+        var game = new Game([a, b, c], 0);
+        game.start();
 
-		if(a.stackAmount > b.stackAmount){
-			assert.equal(a.stackAmount, winner);
-			assert.equal(b.stackAmount, looser);
-		}else{
-			assert.equal(b.stackAmount, winner);
-			assert.equal(a.stackAmount, looser);
-		}
-	});
+        //Pre-Flop
+        c.actions.fold();
+        a.actions.call();
+        b.actions.check();
+        //Flop
+        assert.equal(game.cards.length, 3);
+        a.actions.check();
+        b.actions.bet(2);
+        a.actions.call();
+        //Turn
+        assert.equal(game.cards.length, 4);
+        a.actions.check();
+        b.actions.check();
+        //River
+        assert.equal(game.cards.length, 5);
+        a.actions.check();
+        b.actions.check();
 
-	it("Doit faire gagner le dernier restant si tous le monde se couche avant la river", function(){
-		var a = new Player("Alfred", 10);
-		var b = new Player("Bernard", 10);
-		var c = new Player("Chris", 10);
+        assert.equal(game.pot, 8);
+        var winner = 14;
+        var looser = 6;
 
-		var game = new Game([a, b, c], 0);
-		game.start();
+        if (a.stackAmount > b.stackAmount) {
+            assert.equal(a.stackAmount, winner);
+            assert.equal(b.stackAmount, looser);
+        } else {
+            assert.equal(b.stackAmount, winner);
+            assert.equal(a.stackAmount, looser);
+        }
+    });
 
-		//Pre-Flop
-		c.actions.fold();
-		a.actions.call();
-		b.actions.check();
-		//Flop
-		assert.equal(game.cards.length, 3);
-		assert.equal(game.pot, 4);
-		
-		a.actions.fold();
+    it("Doit faire gagner le dernier restant si tous le monde se couche avant la river", function () {
+        var a = new Player("Alfred", 10);
+        var b = new Player("Bernard", 10);
+        var c = new Player("Chris", 10);
 
-		assert.equal(b.stackAmount, 12);
-	});
+        var game = new Game([a, b, c], 0);
+        game.start();
+
+        //Pre-Flop
+        c.actions.fold();
+        a.actions.call();
+        b.actions.check();
+        //Flop
+        assert.equal(game.cards.length, 3);
+        assert.equal(game.pot, 4);
+
+        a.actions.fold();
+
+        assert.equal(b.stackAmount, 12);
+    });
+
+
+    it.skip('Doit afficher le resultat quand tout le monde est all in', function () {
+        var alfred = new Player("Alfred", 10);
+        var bernard = new Player("Bernard", 10);
+
+        var game = new Game([alfred, bernard], 0);
+        game.start();
+        alfred.actions.bet(20)
+        bernard.actions.call();
+
+        assert.equal(3, game.step)
+    })
+
+    it('Deux joueurs doivent avoir misé la meme chose', function () {
+        var alfred = new Player("Alfred", 10);
+        var bernard = new Player("Bernard", 10);
+        var game = new Game([alfred, bernard], 0);
+        assert(game.everybodyHasBetTheSameAmount([alfred, bernard]))
+    })
+
+    it('Si une personne a relancé, les deux joueurs nont pas misé la même chose', function () {
+        var alfred = new Player("Alfred", 10);
+        alfred.betAmount = 10
+        var bernard = new Player("Bernard", 10);
+        var game = new Game([alfred, bernard], 0);
+        assert(!game.everybodyHasBetTheSameAmount([alfred, bernard]))
+    })
+
+    it('BB tribet shove, SB pussy fold', function () {
+        var alfred = new Player("Alfred", 10);
+        var bernard = new Player("Bernard", 10);
+
+        var game = new Game([alfred, bernard], 0);
+        game.start();
+        alfred.actions.bet(4)
+        bernard.actions.bet(10);
+        alfred.actions.fold();
+
+        assert.equal(0, game.step)
+        assert.equal(14, bernard.stackAmount)
+        assert.equal(6, alfred.stackAmount)
+    })
+
+
+
+
+
 });
